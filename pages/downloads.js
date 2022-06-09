d0626f9cf23a841656d7e08b7235200053ef31d1
MatheusGrilo <matheusgrilo@live.com>
2022-06-09 16:17:16 -0300

    user interface @ downloads

--- pages/downloads.js
+++ pages/downloads.js
@@ -1,7 +1,16 @@
 import Layout from "../components/Views/Layout/Layout";
-import React, { useState } from "react";
-import { ExternalLinkIcon, CloudUploadIcon } from "@heroicons/react/outline";
+import React, { Fragment, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
+import { Popover, Transition, Dialog } from "@headlessui/react";
+import {
+  HiOutlineDocumentAdd,
+  HiOutlineDocumentText,
+  HiLink,
+  HiExternalLink,
+  HiDownload,
+  HiUser,
+  HiCloudUpload,
+} from "react-icons/hi";
 
 import Fuse from "fuse.js";
 
@@ -10,6 +19,15 @@ import dls from "./dl.json";
 export default function Downloads() {
   const { user, isLoading } = useUser();
   const [query, updateQuery] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   const fuse = new Fuse(dls, {
     keys: ["title", "info"],
@@ -24,6 +42,164 @@ export default function Downloads() {
   }
   return (
     <Layout title="Downloads">
+      {/**
+       * Begin of global modal
+       * */}
+      <>
+        {" "}
+        {user && (
+          <Transition appear show={isOpen} as={Fragment}>
+            <Dialog
+              as="div"
+              className="fixed inset-0 z-10 overflow-y-auto"
+              onClose={closeModal}
+            >
+              <div className="min-h-screen px-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0" />
+                </Transition.Child>
+
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                  className="inline-block h-screen align-middle"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-gray-200 dark:bg-gray-700 shadow-xl rounded-2xl">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-800 dark:text-gray-100"
+                    >
+                      Adicionar link
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                          Arquivo(s)
+                        </label>
+                        <div className="relative mb-5 mt-2">
+                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                            <HiOutlineDocumentAdd className="w-6 h-6" />
+                          </div>
+                          <input
+                            id="expiry"
+                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                            placeholder="Ativador do Linux (64-bits)"
+                          />
+                        </div>
+                      </p>
+                    </div>
+
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                          Descrição
+                        </label>
+                        <div className="relative mb-5 mt-2">
+                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                            <HiOutlineDocumentText className="w-6 h-6" />
+                          </div>
+                          <input
+                            id="expiry"
+                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                            placeholder="Ativa permanentemente o Linux XP 11 Pro"
+                          />
+                        </div>
+                      </p>
+                    </div>
+
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                          Link URL
+                        </label>
+                        <div className="relative mb-5 mt-2">
+                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                            <HiLink className="w-6 h-6" />
+                          </div>
+                          <input
+                            id="expiry"
+                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                            placeholder="https://rapidshare.com/grilo"
+                          />
+                        </div>
+                      </p>
+                    </div>
+
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                          Host
+                        </label>
+                        <div className="relative mb-5 mt-2">
+                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                            <HiExternalLink className="w-6 h-6" />
+                          </div>
+                          <input
+                            id="expiry"
+                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                            placeholder="RapidShare"
+                          />
+                        </div>
+                      </p>
+                    </div>
+
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-600 dark:text-gray-300">
+                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                          Uploader
+                        </label>
+                        <div className="relative mb-5 mt-2">
+                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full cursor-not-allowed">
+                            <HiUser className="w-6 h-6" />
+                          </div>
+                          <input
+                            readOnly
+                            id="host"
+                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border capitalize cursor-not-allowed"
+                            value={user.nickname}
+                          />
+                        </div>
+                      </p>
+                    </div>
+
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-100 bg-purple-800 border border-transparent rounded-md hover:bg-purple-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500"
+                        onClick={closeModal}
+                      >
+                        Adicionar
+                      </button>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </div>
+            </Dialog>
+          </Transition>
+        )}
+      </>
+      {/**
+       * End of global modal
+       * */}
       <section className="container mx-auto p-6 font-mono">
         <div className="w-full mb-8 overflow-hidden shadow-lg">
           <div className="w-full overflow-x-auto">
@@ -43,8 +219,18 @@ export default function Downloads() {
                         />
                       </div>
                       <div className="mx-4 flex">
-                        <CloudUploadIcon className="w-6 h-6 mr-1" />
-                        Adicionar link
+                        <button
+                          onClick={openModal}
+                          className={`${
+                            !user
+                              ? "text-red-700 dark:text-red-400 cursor-not-allowed"
+                              : "text-green-700 dark:text-green-400 hover:underline"
+                          } inline-flex `}
+                          rel="noreferrer"
+                        >
+                          <HiCloudUpload className="w-6 h-6 mr-1" />
+                          Adicionar link
+                        </button>
                       </div>
                     </div>
                   </th>
@@ -55,42 +241,143 @@ export default function Downloads() {
               </thead>
 
               <tbody className="bg-white dark:bg-gray-800">
-                {dlResults.map((dl) => {
-                  const { thumb, title, info, link_name, link, svg, _id } = dl;
-
-                  function Icon({ svg }) {
-                    return <svg className="h-16 w-16" />;
-                  }
-                  return (
+                <>
+                  {user && (
                     <tr
                       className="border border-gray-400 bg-gray-100 dark:border-gray-600 text-sm text-gray-600 dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-900"
-                      key={_id}
+                      key="0"
                     >
                       <td className="p-2">
                         <div className="flex items-center text-sm">
                           <div>
                             <p className="font-semibold text-black dark:text-gray-50">
-                              {title}
+                              MediaFire
                             </p>
                             <p className="text-xs text-gray-600 dark:text-gray-300">
-                              {info}
+                              Pasta com todos os arquivos
                             </p>
                           </div>
                         </div>
                       </td>
-                      <td className="px-4 py-3 text-ms font-semibold">
-                        <a
-                          href={link}
-                          target="_blank"
-                          className="inline-flex text-green-700 dark:text-green-400 hover:underline"
-                          rel="noreferrer"
-                        >
-                          <ExternalLinkIcon className="w-6 h-6 mr-1" />
-                          <span>{link_name}</span>
-                        </a>
+                      <td className="px-4 py-3 text-ms font-semibold p-2">
+                        <div className="flex items-center text-sm">
+                          <div>
+                            <p className="font-semibold text-black dark:text-gray-50">
+                              <a
+                                href="https://www.mediafire.com/folder/kauzy8fazwuzs/InfoHouse"
+                                target="_blank"
+                                className="inline-flex text-green-700 dark:text-green-400 hover:underline"
+                                rel="noreferrer"
+                              >
+                                <HiExternalLink className="w-6 h-6 mr-1" />
+                                <span>MediaFire</span>
+                              </a>
+                            </p>
+                            <p className="text-xs text-gray-600 dark:text-gray-300">
+                              Grilo
+                            </p>
+                          </div>
+                        </div>
                       </td>
                     </tr>
-                  );
+                  )}
+                </>
+                {dlResults.map((dl) => {
+                  const {
+                    title,
+                    info,
+                    link_name,
+                    link,
+                    uploader,
+                    _id,
+                    needUser,
+                  } = dl;
+
+                  if (!needUser) {
+                    return (
+                      <tr
+                        className="border border-gray-400 bg-gray-100 dark:border-gray-600 text-sm text-gray-600 dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-900"
+                        key={_id}
+                      >
+                        <td className="p-2">
+                          <div className="flex items-center text-sm">
+                            <div>
+                              <p className="font-semibold text-black dark:text-gray-50">
+                                {title}
+                              </p>
+                              <p className="text-xs text-gray-600 dark:text-gray-300">
+                                {info}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-4 py-3 text-ms font-semibold p-2">
+                          <div className="flex items-center text-sm">
+                            <div>
+                              <p className="font-semibold text-black dark:text-gray-50">
+                                <a
+                                  href={link}
+                                  target="_blank"
+                                  className="inline-flex text-green-700 dark:text-green-400 hover:underline"
+                                  rel="noreferrer"
+                                >
+                                  <HiExternalLink className="w-6 h-6 mr-1" />
+                                  <span>{link_name}</span>
+                                </a>
+                              </p>
+                              <p className="text-xs text-gray-600 dark:text-gray-300">
+                                {uploader}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  } else {
+                    return (
+                      <>
+                        {user && (
+                          <tr
+                            className="border border-gray-400 bg-gray-100 dark:border-gray-600 text-sm text-gray-600 dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-900"
+                            key={_id}
+                          >
+                            <td className="p-2">
+                              <div className="flex items-center text-sm">
+                                <div>
+                                  <p className="font-semibold text-black dark:text-gray-50">
+                                    {title}
+                                  </p>
+                                  <p className="text-xs text-gray-600 dark:text-gray-300">
+                                    {info}
+                                  </p>
+                                </div>
+                              </div>
+                            </td>
+                            <td className="px-4 py-3 text-ms font-semibold p-2">
+                              <div className="flex items-center text-sm">
+                                <div>
+                                  <p className="font-semibold text-black dark:text-gray-50">
+                                    <a
+                                      href={link}
+                                      target="_blank"
+                                      className="inline-flex text-green-700 dark:text-green-400 hover:underline"
+                                      rel="noreferrer"
+                                    >
+                                      <HiExternalLink className="w-6 h-6 mr-1" />
+                                      <span>{link_name}</span>
+                                    </a>
+                                  </p>
+                                  <p className="text-xs text-gray-600 dark:text-gray-300">
+                                    {uploader}
+                                  </p>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        )}
+                      </>
+                    );
+                  }
                 })}
               </tbody>
             </table>
