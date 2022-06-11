a1efe3f0329e7719405cda8e7652b1f24519d09b
MatheusGrilo <matheusgrilo@live.com>
2022-06-11 11:20:39 -0300

    add link form design

--- pages/downloads.js
+++ pages/downloads.js
@@ -8,6 +8,8 @@ import {
   HiLink,
   HiExternalLink,
   HiDownload,
+  HiOutlineGlobe,
+  HiKey,
   HiUser,
   HiCloudUpload,
 } from "react-icons/hi";
@@ -84,112 +86,173 @@ export default function Downloads() {
                   leaveTo="opacity-0 scale-95"
                 >
                   <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-gray-200 dark:bg-gray-700 shadow-xl rounded-2xl">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-gray-800 dark:text-gray-100"
-                    >
-                      Adicionar link
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-600 dark:text-gray-300">
-                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
-                          Arquivo(s)
-                        </label>
-                        <div className="relative mb-5 mt-2">
-                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
-                            <HiOutlineDocumentAdd className="w-6 h-6" />
-                          </div>
-                          <input
-                            id="expiry"
-                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
-                            placeholder="Ativador do Linux (64-bits)"
-                          />
-                        </div>
-                      </p>
-                    </div>
-
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-600 dark:text-gray-300">
-                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
-                          Descrição
-                        </label>
-                        <div className="relative mb-5 mt-2">
-                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
-                            <HiOutlineDocumentText className="w-6 h-6" />
-                          </div>
-                          <input
-                            id="expiry"
-                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
-                            placeholder="Ativa permanentemente o Linux XP 11 Pro"
-                          />
-                        </div>
-                      </p>
-                    </div>
-
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-600 dark:text-gray-300">
-                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
-                          Link URL
-                        </label>
-                        <div className="relative mb-5 mt-2">
-                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
-                            <HiLink className="w-6 h-6" />
-                          </div>
-                          <input
-                            id="expiry"
-                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
-                            placeholder="https://rapidshare.com/grilo"
-                          />
-                        </div>
-                      </p>
-                    </div>
-
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-600 dark:text-gray-300">
-                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
-                          Host
-                        </label>
-                        <div className="relative mb-5 mt-2">
-                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
-                            <HiExternalLink className="w-6 h-6" />
-                          </div>
-                          <input
-                            id="expiry"
-                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
-                            placeholder="RapidShare"
-                          />
-                        </div>
-                      </p>
-                    </div>
-
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-600 dark:text-gray-300">
-                        <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
-                          Uploader
-                        </label>
-                        <div className="relative mb-5 mt-2">
-                          <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full cursor-not-allowed">
-                            <HiUser className="w-6 h-6" />
-                          </div>
-                          <input
-                            readOnly
-                            id="host"
-                            className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border capitalize cursor-not-allowed"
-                            value={user.nickname}
-                          />
-                        </div>
-                      </p>
-                    </div>
-
-                    <div className="mt-4">
-                      <button
-                        type="button"
-                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-100 bg-purple-800 border border-transparent rounded-md hover:bg-purple-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500"
-                        onClick={closeModal}
+                    <form>
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-800 dark:text-gray-100 text-center uppercase"
                       >
-                        Adicionar
-                      </button>
-                    </div>
+                        Adicionar link
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                            Arquivo(s)
+                          </label>
+                          <div className="relative mb-5 mt-2">
+                            <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                              <HiOutlineDocumentAdd className="w-6 h-6" />
+                            </div>
+                            <input
+                              id="title"
+                              name="title"
+                              type="text"
+                              required
+                              className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                              placeholder="Ativador do Linux (64-bits)"
+                            />
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                            Descrição
+                          </label>
+                          <div className="relative mb-5 mt-2">
+                            <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                              <HiOutlineDocumentText className="w-6 h-6" />
+                            </div>
+                            <input
+                              id="description"
+                              name="description"
+                              type="text"
+                              required
+                              className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                              placeholder="Ativa permanentemente o Linux XP 11 Pro"
+                            />
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                            Link URL
+                          </label>
+                          <div className="relative mb-5 mt-2">
+                            <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                              <HiLink className="w-6 h-6" />
+                            </div>
+                            <input
+                              id="url"
+                              name="url"
+                              type="text"
+                              required
+                              className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                              placeholder="https://rapidshare.com/grilo"
+                            />
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                            Host
+                          </label>
+                          <div className="relative mb-5 mt-2">
+                            <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full">
+                              <HiExternalLink className="w-6 h-6" />
+                            </div>
+                            <input
+                              id="host"
+                              name="host"
+                              type="text"
+                              required
+                              className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border"
+                              placeholder="RapidShare"
+                            />
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <label className="text-gray-800 dark:text-gray-300 text-sm font-bold leading-tight tracking-normal">
+                            Uploader
+                          </label>
+                          <div className="relative mb-5 mt-2">
+                            <div className="absolute right-0 text-gray-600 dark:text-gray-300 flex items-center pr-3 h-full cursor-not-allowed">
+                              <HiUser className="w-6 h-6" />
+                            </div>
+                            <input
+                              readOnly
+                              id="host"
+                              className="text-gray-600 dark:text-gray-300 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-400 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 dark:border-gray-500 rounded border capitalize cursor-not-allowed"
+                              value={user.nickname}
+                            />
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-600 dark:text-gray-300">
+                          <div className="relative mb-5 mt-2 w-full">
+                            <div className="flex items-start mb-6">
+                              <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 w-1/2">
+                                Link privado/público
+                              </label>
+                              <div className="center">
+                                <div className="grid grid-cols-2 xl:grid-cols-2 gap-2 w-full">
+                                  <div className="inline-block radio">
+                                    <input
+                                      name="userOnly"
+                                      type="radio"
+                                      id="userOnlyYes"
+                                      value="1"
+                                      hidden="hidden"
+                                      className="peer"
+                                    />
+                                    <label
+                                      htmlFor="userOnlyYes"
+                                      className="peer-checked:text-red-700 dark:peer-checked:text-red-400"
+                                    >
+                                      <HiKey className="px-2 py-1 rounded-lg flex justify-center items-center text-3xl lg:text-5xl font-bold w-10 h-10 lg:w-14 lg:h-14 cursor-pointer" />
+                                    </label>
+                                  </div>
+                                  <div className="inline-block radio">
+                                    <input
+                                      name="userOnly"
+                                      type="radio"
+                                      id="userOnlyNo"
+                                      hidden="hidden"
+                                      value="0"
+                                      className="peer"
+                                    />
+                                    <label
+                                      htmlFor="userOnlyNo"
+                                      className="peer-checked:text-green-700 dark:peer-checked:text-green-400"
+                                    >
+                                      <HiOutlineGlobe className="px-2 py-1 rounded-lg flex justify-center items-center text-3xl lg:text-5xl font-bold w-10 h-10 lg:w-14 lg:h-14 cursor-pointer" />
+                                    </label>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </p>
+                      </div>
+
+                      <div className="mt-4">
+                        <button
+                          type="submit"
+                          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-100 bg-purple-800 border border-transparent rounded-md hover:bg-purple-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500"
+                        >
+                          Adicionar
+                        </button>
+                      </div>
+                    </form>
                   </div>
                 </Transition.Child>
               </div>
